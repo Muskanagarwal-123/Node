546d3d39809b482c216657d0f358452b023cc789
Muskanagarwal-123 <amuskan076@gmail.com>
2022-07-14 23:10:47 +0530

    deleting unwanted files

--- Routes/student.js
+++ Routes/student.js
@@ -25,9 +25,8 @@ studentRouter.delete('/info/:id', async (req, res) => {
 studentRouter.put("/info/:id",async(req,res)=> {
   let id=req.params.id;
   const query = {_id:id};
-  await StudentModel.updateOne(
-    (query),
-    {StudentName: req.body.StudentName, Class:req.body.Class});
+  const update={...req.body};
+  await StudentModel.updateOne((query),update);
   res.send("Record updated successfully");
 });
 
